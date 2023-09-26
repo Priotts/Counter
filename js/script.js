7c4d360e16f80cdc5f34ffc47197f4ce62d4508c
Priotts <hptktrs@gmail.com>
2023-09-26 20:16:10 +0200

    Removed the event listeners on the buttons and added Event Delegation on the btnBox

--- js/script.js
+++ js/script.js
@@ -1,55 +1,62 @@
 function increase(result, resultElement) {
-    result += 1;
-    resultElement.textContent = result;
-    return result;
-  }
-  
-  function decrease(result, resultElement) {
-    result -= 1;
-    resultElement.textContent = result;
-    return result;
-  }
-  
-  function reset(result, resultElement) {
-    result = 0;
-    resultElement.textContent = result;
-    return result;
-  }
-  
-  function createButton(id, text, clickHandler) {
-    let button = document.createElement('button');
-    button.setAttribute('id', id);
-    button.textContent = text;
-    button.addEventListener('click', clickHandler);
-    return button;
-  }
-  
-  function addButtons() {
+  result += 1;
+  resultElement.textContent = result;
+  return result;
+}
+
+function decrease(result, resultElement) {
+  result -= 1;
+  resultElement.textContent = result;
+  return result;
+}
+
+function reset(result, resultElement) {
+  result = 0;
+  resultElement.textContent = result;
+  return result;
+}
+
+function createButton(id, text) {
+  let button = document.createElement('button');
+  button.setAttribute('id', id);
+  button.textContent = text;
+  return button;
+}
+
+function addButtons() {
     let result = 0;
     let resultElement = document.createElement('div');
     resultElement.setAttribute('id', 'resultBox');
     resultElement.textContent = result;
     let btnBox = document.querySelector('.btn-box');
-    
-    let incBtn = createButton('increaseBtn', 'Increase', function () {
-      result = increase(result, resultElement);
-    });
-  
-    let decBtn = createButton('decreaseBtn', 'Decrease', function () {
-      result = decrease(result, resultElement);
-    });
-  
-    let resBtn = createButton('resetBtn', 'Reset', function () {
-      result = reset(result, resultElement);
-    });
-  
+
+    let incBtn = createButton('increaseBtn', 'Increase');
+    let decBtn = createButton('decreaseBtn', 'Decrease');
+    let resBtn = createButton('resetBtn', 'Reset');
+
+    btnBox.addEventListener('click', event =>{
+      let target = event.target;
+      if (target.id === 'increaseBtn'){
+        console.log('increase');
+        result = increase(result, resultElement);
+      }
+      if (target.id === 'decreaseBtn'){
+        console.log('decrease');
+        result = decrease(result, resultElement);
+      }
+      if (target.id === 'resetBtn'){
+        console.log('reset')
+        result = reset(result, resultElement);
+      }
+    })
+
     btnBox.append(incBtn, resultElement, decBtn, resBtn);
     btnBox.classList.add('show');
-    
+
     // Remove the event listener
     start.removeEventListener('click', addButtons);
     start.textContent = 'ENJOY!';
   }
-  
-  let start = document.getElementById('start');
-  start.addEventListener('click', addButtons);
+
+let start = document.getElementById('start');
+start.addEventListener('click', addButtons);
